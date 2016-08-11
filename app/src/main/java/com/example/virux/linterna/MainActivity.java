403c795e8a5701c312f83f95034b88f48e45e6e5
Oscar Duarte <leodufer@gmail.com>
2016-08-10 20:53:05 -0400

    se agrego boton de encender y apagar

--- app/src/main/java/com/example/virux/linterna/MainActivity.java
+++ app/src/main/java/com/example/virux/linterna/MainActivity.java
@@ -2,24 +2,48 @@ package com.example.virux.linterna;
 
 
 import android.hardware.Camera;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.hardware.Camera.Parameters;
-
-
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Camera camera;
+    Button encenderButton;
+
+    Boolean apagado = true;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        encenderButton = (Button) findViewById(R.id.buttonEncender);
         camera = Camera.open();
-        Parameters parameters = camera.getParameters();
-        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-        camera.setParameters(parameters);
+
+        encenderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Parameters parameters = camera.getParameters();
+
+                if(apagado){
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                }else{
+                    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                }
+                camera.setParameters(parameters);
+                //camera.startPreview();
+
+
+
+            }
+        });
+
+
     }
 }
